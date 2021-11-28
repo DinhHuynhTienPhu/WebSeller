a1000a94e38b92e981e6fd29ae6dd7df85398d9c
dinhhuynhtienphu <ff10111011@gmail.com>
2021-11-28 12:25:53 +0700

    a

--- Components/product/productController.js
+++ Components/product/productController.js
@@ -8,9 +8,9 @@ exports.ShowList = async (req, res) => {
   let page = req.query.page;; //mặc định là page 1, nếu như link là http://localhost:3003/product/product-list?page=2 thì là page 2
   if (isNaN(page)) page = 1;
   console.log("page= " + page);
-  let data = await service.ShowList(page, itemPerPage)
+  //let data = await service.ShowList(page, itemPerPage)
   res.render("../Components/product/product-list", {
-    data: data
+  //  data: data
   });
 }
 
